d792b9671548b203c65fc92ed704dbf6db39fcb0
msoliman5 <54380486+msoliman5@users.noreply.github.com>
2019-09-22 21:47:49 -0700

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setupConnections();
+    setupconnections();
 }
 
 MainWindow::~MainWindow()
@@ -14,20 +14,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::changePageToContactUs()
+void MainWindow::changepageToContactUs()
 {
-    ui->stackedWidget->setCurrentWidget(ui->ContactPage);
+    ui->stackedWidget->setCurrentWidget(ui->Contactpage);
 }
-
 void MainWindow::changePageToHome()
 {
-    ui->stackedWidget->setCurrentWidget(ui->HomePage);
+    ui->stackedWidget->setCurrentWidget(ui->homePage);
 }
-
-void MainWindow::setupConnections()
+void MainWindow::changepagetobuynow()
 {
-    connect(ui->btnContact,&QPushButton::clicked,
-            this,&MainWindow::changePageToContactUs);
-    connect(ui->btnHomePage,&QPushButton::clicked,
-            this,&MainWindow::changePageToHome);
+    ui->stackedWidget->setCurrentWidget(ui->buynowpage);
+}
+void MainWindow::changetocartpage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->Cartpage);
+}
+void MainWindow::changetoGuaranteepage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->Guaranteepage);
+}
+void MainWindow::changetocustomerreviewspage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->Customersreview);
+}
+void MainWindow::changetohelppage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->Helppage);
+}
+void MainWindow::setupconnections()
+{
+    connect(ui->Contactus,&QPushButton::clicked,this,&MainWindow::changepageToContactUs);
+    connect(ui->Homebutton,&QPushButton::clicked,this,&MainWindow::changePageToHome);
+    connect(ui->Buynow,&QPushButton::clicked,this,&MainWindow::changepagetobuynow);
+    connect(ui->backbutton,&QPushButton::clicked,this,&MainWindow::changePageToHome);
+    connect(ui->cartButton,&QPushButton::clicked,this,&MainWindow::changetoGuaranteepage);
+    connect(ui->cancelbutton,&QPushButton::clicked,this,&MainWindow::changePageToHome);
+    connect(ui->declinebutton,&QPushButton::clicked,this,&MainWindow::changePageToHome);
+    connect(ui->Acknowledgebutton,&QPushButton::clicked,this,&MainWindow::changetocartpage);
+    connect(ui->Customerreviewsbutton,&QPushButton::clicked,this,&MainWindow::changetocustomerreviewspage);
+    connect(ui->closebtn,&QPushButton::clicked,this,&MainWindow::changePageToHome);
+    connect(ui->Helpbutton,&QPushButton::clicked,this,&MainWindow::changetohelppage);
+    connect(ui->helphomebtn,&QPushButton::clicked,this,&MainWindow::changePageToHome);
 }
